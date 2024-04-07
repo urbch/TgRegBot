6cac6331509f019b5eb4866e2846910d85283b48
Alina Anisimova <anisimovaa3.1415926@gmail.com>
2024-04-07 20:55:18 +0300

    Some changes

--- Routers/Commands/base_commands.py
+++ Routers/Commands/base_commands.py
@@ -8,34 +8,40 @@ from keyboards.start_keyboard import ButtonText, get_on_start_kb
 
 router = Router(name=__name__)
 
+
 @router.message(CommandStart())
 async def process_start(message: Message):
     await message.answer(
-        text = f"Привет, {markdown.hbold(message.from_user.full_name)}!",
-        parse_mode=ParseMode.HTML,
+        text=f"""Здравствуйте, {markdown.hbold(message.from_user.full_name)}!
+                   Выберите подходящий пункт в меню""",
         reply_markup=get_on_start_kb()
     )
-@router.message(F.text == ButtonText.WHATS_NEXT)
+
+
 @router.message(Command("help"))
 async def process_help(message: Message):
-    text = markdown.text(
-        markdown.markdown_decoration.quote("I'm a test bot."),
-        markdown.text(
-            "Send me",
-            markdown.markdown_decoration.bold(
-                markdown.text(
-                    markdown.underline("literally"),
-                     "any",
-                 ),
-            ),
-            markdown.markdown_decoration.quote("message!"),
-        ),
-        sep="\n",
-    )
-    await message.answer(text = text, parse_mode = ParseMode.MARKDOWN_V2)
+    text = "Данный бот предназначен для регистрации участников квизов на викторины"
+    await message.answer(text=text)
 
-@router.message(Command("info", prefix="!/"))
-async def handle_info(message: Message):
+
+@router.message(F.text == ButtonText.REG)
+async def handle_reg_button(message: Message):
+    date1 = InlineKeyboardButton(text="1")
+    date2 = InlineKeyboardButton(text="2")
+    date3 = InlineKeyboardButton(text="3")
+    row1 = [date1]
+    row2 = [date2]
+    row3 = [date3]
+    rows = [row1, row2, row3]
+    markup = InlineKeyboardMarkup(inline_keyboard=rows)
+    await message.amswer(
+        text="Выберите подохдящую дату: ",
+        reply_markup=markup,
+    )
+
+
+@router.message(Command("support", prefix="!/"))
+async def handle_support(message: Message):
     tg_channel_button = InlineKeyboardButton(text="Канал в телеграме",
                                              url="https://skillbox.ru/media/code/biblioteka-matplotlib-dlya-postroeniya-grafikov")
     row = [tg_channel_button]
--- Routers/common.py
+++ Routers/common.py
@@ -5,20 +5,12 @@ from keyboards.start_keyboard import ButtonText
 
 router = Router(name=__name__)
 
-@router.message(F.text == ButtonText.BYE)
-async def handle_bye_message(mesage: Message):
-    await mesage.answer(
-        text="See you later, click /start any time!",
-        reply_markup=ReplyKeyboardRemove()
-    )
-
 @router.message()
 async def process_message(message: Message):
-
     await message.answer(
-        text = "Wait a second..."
+        text="К сожалению, я Вас не понимаю. Выберите пункт из меню."
     )
     try:
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        await message.answer("Something new...")
+        await message.answer("К сожалению, я Вас не понимаю. Выберите пункт из меню")
