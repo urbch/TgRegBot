8bc0766ffbb46e70a26a2021ce9914c2b026b1c6
Liliana Smirnikova <lilianasmir@icloud.com>
2024-04-09 12:05:31 +0300

    reg_hand2

--- Routers/Commands/__init__.py
+++ Routers/Commands/__init__.py
@@ -4,7 +4,8 @@ from aiogram import Router
 
 from .base_commands import router as base_commands_router
 from .user_commands import router as user_commands_router
+from .reg_handler import router as reg_handler_router
 
 router = Router(name=__name__)
 
-router.include_routers(base_commands_router, user_commands_router)
+router.include_routers(base_commands_router, user_commands_router, reg_handler_router)
